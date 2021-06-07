fe6f5cbc184ecdac4aab78f344e9d20a3322d3a9
Dani <dga_3@hotmail.com>
2021-06-07 20:45:02 +0100

    init

---
+++ SampleTest1/SampleTest1UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SampleTest1
+{
+    [TestClass]
+    public class SampleTest1UnitTest1
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext { get => testContextInstance; set => testContextInstance = value; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RhinoInside.Resolver.Initialize();
+            //AppDomain.CurrentDomain.AssemblyResolve += Tenrec.RhinoHeadless.ResolveAssemblies;
+            Tenrec.Utils.RunTenrecGroups(System.IO.Path.Combine(System.IO.Directory.GetParent(typeof(Tenrec.Utils).Assembly.Location).Parent.Parent.Parent.FullName, "TenrecSAMPLETest.gh"), TestContext);
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,18 +4,18 @@ using System;
 namespace Tenrec.Tests
 {
     [TestClass]
-    public class UnitTestSAMPLE
+    public class UnitTest1
     {
         private TestContext testContextInstance;
         public TestContext TestContext { get => testContextInstance; set => testContextInstance = value; }
 
         [TestMethod]
-        public void Test_TestGroup1()
+        public void RhinoInside_RunGrasshopperFile()
         {
             Utils.RunTenrecGroups(System.IO.Path.Combine(System.IO.Directory.GetParent(typeof(Utils).Assembly.Location).Parent.FullName, "TenrecSAMPLETest.gh"), TestContext);
         }
         [TestMethod]
-        public void Test_TestGrasshopper()
+        public void RhinoInside_CreateGHPoint_IsAsExpected()
         {
             var pt = new Grasshopper.Kernel.Types.GH_Point(new Rhino.Geometry.Point3d(1, 1, 1));
             Assert.AreEqual(pt.Value, new Rhino.Geometry.Point3d(1, 1, 1));
